1521038e0a136b56ad8c6e290232fc341d05873b
kayua <kayuaolequesp@gmail.com>
2021-03-11 09:20:29 -0300

    example deep_neural_network_naive

--- 5_clusters_basico_ao_avancado/main.py
+++ 5_clusters_basico_ao_avancado/main.py
@@ -3,24 +3,36 @@
 from pandas import read_csv
 
 # Carregamento dos dados da tabela
-from sklearn.cluster import KMeans, MeanShift, DBSCAN, OPTICS
-from sklearn.neighbors import NearestNeighbors
+from sklearn.cluster import KMeans
+from sklearn.cluster import MeanShift
+from sklearn.cluster import DBSCAN
+from sklearn.cluster import OPTICS
+from sklearn.cluster import AgglomerativeClustering
+from sklearn.mixture import GaussianMixture
 
+# Carregamento dos dados da tabela
 dados_tabela = read_csv('tabela/dataset_iris.csv', usecols=[1, 2, 3, 4], engine='python')
 rotulos = read_csv('tabela/dataset_iris.csv', usecols=[0], engine='python')
 conjunto_de_dados = dados_tabela.values
 rotulos = rotulos.values
+
 # Conversão para float32
 conjunto_de_dados = conjunto_de_dados.astype('float32')
-dados_para_predicao = conjunto_de_dados[0:len(conjunto_de_dados), :]
-
 rotulos = rotulos.astype('int32')
+
+# recorte dos dados
+dados_para_predicao = conjunto_de_dados[0:len(conjunto_de_dados), :]
 rotulos = rotulos[0:len(rotulos), :]
 
-#resultados = KMeans(n_clusters=3, random_state=10).fit_predict(dados_para_predicao)
+
+# Aqui estão alguns algoritmos de clustering mais conhecidos
+# Para utilizar um deles basta remover o comentário do algoritmo desejado e comentar os demais
+resultados = KMeans(n_clusters=3, random_state=10).fit_predict(dados_para_predicao)
 #resultados = MeanShift(0.8).fit_predict(dados_para_predicao)
+#resultados = AgglomerativeClustering(n_clusters=3, affinity='euclidean').fit_predict(dados_para_predicao)
 #resultados = DBSCAN(0.7).fit_predict(dados_para_predicao)
-resultados = OPTICS(min_samples=20, xi=.05, min_cluster_size=.05).fit_predict(dados_para_predicao)
+#resultados = OPTICS(min_samples=20, xi=.05, min_cluster_size=.05).fit_predict(dados_para_predicao)
+#resultados = GaussianMixture(n_components=3, covariance_type='full').fit_predict(dados_para_predicao)
 
 for i in range(len(resultados)):
 
